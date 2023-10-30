cd1cf1c4ce0368aa45d209f04417694c9da3c4a3
ctrl-ale-del <148604407+ctrl-ale-del@users.noreply.github.com>
2023-10-30 16:52:58 -0700

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -1,6 +1,20 @@
 //Runs before the javascript in the html file
 
-//Console.log prints to console 
+const navToggle = document.querySelector('.nav-toggle');
+const links = docment.querySelector('.links');
+
+navToggle.addEventListener('click', function() {
+    /*console.log(links.classList.contains("random"));
+    console.log(links.classList.contains('Links'));
+    if (links.classList.contains("show-links")){
+        links.classList.remove("show-links");
+    } else {
+        links.classList.add("show-links");
+    }*/
+    links.classList.toggle("show-links");
+})
+
+/*Console.log prints to console 
 console.log("Hello World")
 console.log(23 + 98)
 console.log((4 + 6 + 9) / 77)
@@ -12,5 +26,5 @@ actual = max - 13
 percentage = actual / max
 console.log(percentage)
 
-//Creates a pop up with a message
-//window.alert("Hello World")
+Creates a pop up with a message
+window.alert("Hello World") */
