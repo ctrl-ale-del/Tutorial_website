b3e1d6c6a41d0cf7d9f0f643790a1ac81f5b263e
ctrl-ale-del <148604407+ctrl-ale-del@users.noreply.github.com>
2023-10-30 15:28:24 -0700

    Create tutorial.js

---
+++ tutorial.js
@@ -0,0 +1,16 @@
+//Runs before the javascript in the html file
+
+//Console.log prints to console 
+console.log("Hello World")
+console.log(23 + 98)
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+console.log(a)
+const max = 57
+actual = max - 13
+percentage = actual / max
+console.log(percentage)
+
+//Creates a pop up with a message
+//window.alert("Hello World")
